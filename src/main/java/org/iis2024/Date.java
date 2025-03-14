e5f8cdde1fd7fbeef16441513e69f518f91ebac6
Ralsei22 <pablomongebellido22@gmail.com>
2025-03-14 13:06:40 +0100

    Conlfict

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -15,12 +15,11 @@ import java.text.SimpleDateFormat;
  * Instances of this class are immutable, meaning that once a Date object is created, the day,
  * month, and year values cannot be changed.
  */
-public class Date {
-  private int fieldAdded;
-  private final int day;
-  private final int month;
-  private final int year;
-
+// Constructor modified by the owner
+public Date(int dia, int mes, int año) {
+  this.day = dia;
+  this.month = mes;
+  this.year = año;
   /**
    * Retrieves the day component of this date.
    *
@@ -89,4 +88,4 @@ public class Date {
   public void methodAddedByOwner() {
     System.out.println("Method added by owner");
   }
-}
+  }
